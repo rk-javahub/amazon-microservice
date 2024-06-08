101a0e223b4869b8c0a53c4b8e43d15f2ba32267
Rohit <kumbharrohit13@gmail.com>
2024-06-08 13:19:11 +0530

    Added code

--- inventory-service/src/main/java/com/rkjavahub/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/rkjavahub/inventoryservice/repository/InventoryRepository.java
@@ -2,9 +2,11 @@ package com.rkjavahub.inventoryservice.repository;
 
 import com.rkjavahub.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     Optional<Inventory> findBySkuCode(String skuCode);
 }
--- order-service/order-service/src/main/java/com/rkjavahub/orderservice/repository/OrderRepository.java
+++ order-service/order-service/src/main/java/com/rkjavahub/orderservice/repository/OrderRepository.java
@@ -3,5 +3,6 @@ package com.rkjavahub.orderservice.repository;
 import com.rkjavahub.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
