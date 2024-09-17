cfcddaeb9ce22f189ca46ecb5233c6d9fb0d3e1a
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-09-17 13:28:05 +0530

    circuitbracker properties added

--- order-service/src/main/java/com/rkjavahub/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/rkjavahub/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.rkjavahub.orderservice.controller;
 
 import com.rkjavahub.orderservice.dto.OrderRequest;
 import com.rkjavahub.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! Something went wrong! Please order after some time.";
     }
 }
